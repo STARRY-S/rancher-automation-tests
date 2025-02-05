77b2aeb3e4587ca57778812f1dc1208b0d10f839
StarryWang <starry.wang@suse.com>
2025-02-05 18:10:30 +0800

    Update to add daily check

---
+++ scripts/ci/check.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Check public cloud remaining resources
+
+cd $(dirname $0)/../../
+
+set -euo pipefail
+
+# Build checker cli
+./scripts/build.sh
+
+CLOUDS=(
+    "aws"
+    "awscn"
+    "hwcloud"
+    # "tencent" # TODO: add tencent support
+    # "aliyun" # TODO: add aliyun support
+)
+
+for CLOUD in ${CLOUDS[@]}; do
+    PROVIDER=$CLOUD
+    if [[ $CLOUD = "aws" ]]; then
+        export AWS_AK="$AWS_AK"
+        export AWS_SK="$AWS_SK"
+        export AWS_REGION="$AWSCN_REGION"
+    elif [[ $CLOUD = "awscn" ]]; then
+        export AWS_AK="$AWSCN_AK"
+        export AWS_SK="$AWSCN_SK"
+        export AWS_REGION="$AWSCN_REGION"
+        PROVIDER="aws"
+    fi
+
+    echo "Checking resources of cloud [$CLOUD]"
+    ./checker $PROVIDER \
+        --filter="auto-rancher-" \
+        --filter="oetest" \
+        --filter="auto-aws-" \
+        --filter="starry" \
+        --filter="eip" \
+        --output="remain-resources.txt" \
+        --auto-yes
+
+    if [[ -e "remain-resources.txt" ]]; then
+        echo "-----------------------------"
+        echo "$CLOUD resources:"
+        cat remain-resources.txt
+        echo
+        echo "There are unstopped resources on $CLOUD" >&2
+        echo "Please check manually!" >&2
+        echo
+        echo "CHECK: FAILED" >&2
+        exit 1
+    fi
+done
+
+echo "CHECK: PASS"
